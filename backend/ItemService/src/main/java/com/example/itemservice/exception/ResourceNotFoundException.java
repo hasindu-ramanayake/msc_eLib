a573bc0bd4de908cf5801f8aeb0f838a435139a8
Corey Shanahan <coreyshanahan01@gmail.com>
2026-04-07 11:58:51 +0100

    intergrate item service to project

--- backend/ItemService/src/main/java/com/example/itemservice/exception/ResourceNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.example.itemservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String msg){ super(msg);}
-}
--- backend/ItemService/src/main/java/com/example/itemservice/mapper/BaseMapper.java
+++
@@ -1,6 +0,0 @@
-package com.example.itemservice.mapper;
-
-public interface BaseMapper<D, E> {
-    D toDto(E entity);
-    E toEntity(D dto);
-}
