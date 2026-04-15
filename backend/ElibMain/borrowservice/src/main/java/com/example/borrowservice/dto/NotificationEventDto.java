86c9baa76eaefab50a5114627d4b508ba0975450
Corey Shanahan <coreyshanahan01@gmail.com>
2026-04-15 17:46:31 +0100

    [borrow_service] pass jwt to notification queue

--- backend/ElibMain/borrowservice/src/main/java/com/example/borrowservice/dto/NotificationEventDto.java
+++ backend/ElibMain/borrowservice/src/main/java/com/example/borrowservice/dto/NotificationEventDto.java
@@ -3,6 +3,7 @@ package com.example.borrowservice.dto;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -13,4 +14,6 @@ public class NotificationEventDto {
     private UUID userId;
     private Map<String, String> payload;
     private Date occuredAt;
+    private String jwtToken;
+    private List<String> notificationPreferences;
 }
