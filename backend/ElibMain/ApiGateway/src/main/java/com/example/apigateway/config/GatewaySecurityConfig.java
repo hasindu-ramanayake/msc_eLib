0b10f7dcb9b471b734c4f05b3f6edf14ae1d4e7f
Jack <farrelljack741@gmail.com>
2026-04-22 12:58:00 +0100

    Minor cors change

--- backend/ElibMain/ApiGateway/src/main/java/com/example/apigateway/config/GatewaySecurityConfig.java
+++ backend/ElibMain/ApiGateway/src/main/java/com/example/apigateway/config/GatewaySecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.apigateway.filter.AuthenticationFilter;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -29,7 +30,7 @@ public class GatewaySecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> {})
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .anonymous(anonymous -> anonymous.authorities("ROLE_ANONYMOUS"))
